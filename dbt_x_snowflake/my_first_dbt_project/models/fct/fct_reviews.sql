7a5f305267299a60b3deeede891c2f1ff842fdbc
adrian-pasek-im <pasek.adrian.314@gmail.com>
2022-07-14 13:28:58 +0200

    Add materializations, seeds, and sources

---
+++ dbt_x_snowflake/my_first_dbt_project/models/fct/fct_reviews.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change = 'fail'
+    )
+}}
+-- materialized = 'incremental' -> append rows in this model
+-- on_schema_change = 'fail' -> since table is not recreated, the schema is set and if it changes somehow we need to react
+WITH src_reviews AS (
+    SELECT * FROM {{ ref('src_reviews') }}
+)
+
+SELECT * FROM src_reviews
+WHERE review_text IS NOT NULL
+-- if materialized is incremental then add AND clause which states that retrive only rows where review_date is after max(review_date) of the model {{ this }}
+{% if is_incremental() %}
+    AND review_date > (SELECT MAX(review_date) FROM {{ this }})
+{% endif %}
