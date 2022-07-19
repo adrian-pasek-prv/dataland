e18406c0f86738e6d184fd959320a02c37a6821d
adrian-pasek-im <pasek.adrian.314@gmail.com>
2022-07-19 07:36:15 +0200

    Add custom packages, documentation

--- dbt_x_snowflake/my_first_dbt_project/models/fct/fct_reviews.sql
+++ dbt_x_snowflake/my_first_dbt_project/models/fct/fct_reviews.sql
@@ -10,7 +10,9 @@ WITH src_reviews AS (
     SELECT * FROM {{ ref('src_reviews') }}
 )
 
-SELECT * FROM src_reviews
+SELECT {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} as review_id,
+        *
+FROM src_reviews
 WHERE review_text IS NOT NULL
 -- if materialized is incremental then add AND clause which states that retrive only rows where review_date is after max(review_date) of the model {{ this }}
 {% if is_incremental() %}
